553d41e6c915385c59a50bf69c2637612e667643
SantigBenites <santi.mbenites@gmail.com>
2025-10-13 16:22:32 +0100

    finisehd 2 pointers

--- 2_pointers/rain_water.py
+++ 2_pointers/rain_water.py
@@ -6,5 +6,17 @@ from typing import List
 
 def trap(height: List[int]) -> int:
 
+    water_level = []
+    for idx,x in enumerate(height):
+
+        prefix = max(height[idx:]) if len(height[idx:]) > 0 else 0 
+        suffix = max(height[:idx]) if len(height[:idx]) > 0 else 0
+        current_level = min(prefix, suffix) - x
+        if current_level > 0:
+            water_level.append(current_level)
+
+    return sum(water_level)
 
 
+height = [0,2,0,3,1,0,1,3,2,1]
+print(trap(height))
